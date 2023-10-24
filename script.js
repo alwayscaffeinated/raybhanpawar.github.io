31d0ca9b922f7958afb09f7645d9c1e1c2f324a4
alwayscaffeinated <52075063+alwayscaffeinated@users.noreply.github.com>
2023-10-24 17:05:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function buttonAction(action) {
+    switch (action) {
+        case 'contact':
+            // Code for what happens when "contact me" button is clicked
+            alert('Contact Me button clicked!');
+            break;
+        case 'portfolio':
+            // Code for what happens when "browse portfolio" button is clicked
+            alert('Browse Portfolio button clicked!');
+            break;
+    }
+}
+
+
